8ecd1ce88d6e3fa8a3d94e1f3dd8b860d3ec2722
dafalagi <dafarizky34@gmail.com>
2023-01-18 23:46:59 +0700

    fixed routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ContentController;
+use App\Http\Controllers\FavoriteController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -21,5 +24,13 @@ Route::post('/register', [AuthController::class, 'register']);
 
 // AUTHENTICATED ONLY
 Route::middleware('auth:sanctum')->group(function () {
+    // POST
     Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth.default');
+
+    // RESOURCES
+    Route::apiResources([
+        'user' => UserController::class,
+        'content' => ContentController::class,
+        'favorite' => FavoriteController::class,
+    ]);
 });
